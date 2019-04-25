57bf203e19d9209d532723f45efa2bdd1360a0b1
Stephen Cooper <Stephen.Cooper@cascadehr.co.uk>
2019-04-25 14:46:45 +0100

    added extra command line args

--- app/Components/releaseNotesViewer/releaseNotesViewer.js
+++ app/Components/releaseNotesViewer/releaseNotesViewer.js
@@ -1,8 +1,11 @@
 const WebComponentBase = require('../WebComponentBase'),
     md = require('markdown-it')(),
     fs = require('fs'),
-    path = require('path');
-
+    path = require('path'),
+    markdownItAttrs = require('markdown-it-attrs');
+ 
+md.use(markdownItAttrs);
+     
 /**
  * Parses and display the Release Notes MD file.
  */
@@ -15,13 +18,13 @@ module.exports = class ReleaseNotesViewer extends WebComponentBase {
 
         this.applyHTML(shadow, __dirname, 'releaseNotes.html');
 
+        this.applyStyle(shadow, __dirname, 'releaseNotes.css');
+
         const releaseNotesContents = fs.readFileSync(path.resolve(__dirname, '../../../ReleaseNotes.md')).toString();
 
         const releaseNotes = shadow.querySelector('.release-notes');
 
         releaseNotes.innerHTML = md.render(releaseNotesContents);
-
-        Array.from(releaseNotes.querySelectorAll('table')).forEach(x => x.classList.add('table'));
     }
 
     static register() {
